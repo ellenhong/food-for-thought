9bb99c89952bd4a50623538a5b516e148e1a100f
Ellen Hong <e.hong@berkeley.edu>
2015-11-30 21:56:08 -0800

    done with blog section

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,18 @@ $(document).ready(function() {
 
 var container = document.querySelector('#masonry-grid');
 var masonry = new Masonry(container, {
-columnWidth: 300,
-gutter: 15,
-itemSelector: '.grid-item'
+	columnWidth: 300,
+	gutter: 15,
+	itemSelector: '.grid-item'
 });
+
 
 $("a").click(function() {
     $('html, body').animate({
     scrollTop: $( $.attr(this, 'href')).offset().top
     }, 1500);
     return false;
-  })
+});
 
 
 $(".darken").hover(function() {
@@ -21,14 +22,12 @@ $(".darken").hover(function() {
     $(this).find("img").fadeTo(500, 0.7);
 });
 
-$('.darken').hover(
-    function(){
-       $(this).find('.message').fadeIn(1000); 
-    },
-    function(){
-       $(this).find('.message').fadeOut(1000); 
-    }
-    );
+$('.darken').hover(function(){
+   $(this).find('.message').fadeIn(1000); 
+}, function(){
+   $(this).find('.message').fadeOut(1000); 
+});
+
 
 $("#link1").mouseover(function() {
 	$("#pop-up1").show();
@@ -50,5 +49,6 @@ $("#link3").mouseover(function() {
 $("#link3").mouseout(function() {
 	$("#pop-up3").hide();
 });
+
 
 })
